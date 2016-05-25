60aa1156e47f63583d574a9c107d5d391d6e55a6
Marco <marco1@live.de>
2016-05-25 09:03:25 +0200

    Change spaces to tabs as per '.editorconfig'

--- _js/custom.js
+++ _js/custom.js
@@ -1,109 +1,109 @@
 (function ($) {
 
-    "use strict";
+	"use strict";
 
-    var javadocToMarkdown = new JavadocToMarkdown();
+	var javadocToMarkdown = new JavadocToMarkdown();
 
-    function startConverter() {
-        $("div.jumbotron").slideUp(400);
-    }
+	function startConverter() {
+		$("div.jumbotron").slideUp(400);
+	}
 
-    function showConvertedCode(language, headingsLevel, code) {
-        var output,
-            outputBox;
+	function showConvertedCode(language, headingsLevel, code) {
+		var output,
+			outputBox;
 
-        outputBox = $("#results");
+		outputBox = $("#results");
 
-        switch (language) {
-            case "Javadoc": output = javadocToMarkdown.fromJavadoc(code, headingsLevel); break;
-            case "PHPDoc": output = javadocToMarkdown.fromPHPDoc(code, headingsLevel); break;
-            case "JSDoc": output = javadocToMarkdown.fromJSDoc(code, headingsLevel); break;
-            default: throw "Unsupported language "+language;
-        }
+		switch (language) {
+			case "Javadoc": output = javadocToMarkdown.fromJavadoc(code, headingsLevel); break;
+			case "PHPDoc": output = javadocToMarkdown.fromPHPDoc(code, headingsLevel); break;
+			case "JSDoc": output = javadocToMarkdown.fromJSDoc(code, headingsLevel); break;
+			default: throw "Unsupported language "+language;
+		}
 
-        outputBox.text(output);
-        outputBox.parent().slideDown(400);
-    }
+		outputBox.text(output);
+		outputBox.parent().slideDown(400);
+	}
 
-    function setLanguage(language) {
-        $("label[for='javadoc-to-markdown-code']").text(language);
-        $("#javadoc-to-markdown-code").attr("placeholder", "Paste your code that contains "+language+" ...");
-    }
+	function setLanguage(language) {
+		$("label[for='javadoc-to-markdown-code']").text(language);
+		$("#javadoc-to-markdown-code").attr("placeholder", "Paste your code that contains "+language+" ...");
+	}
 
-    $(document).ready(function () {
-        $("textarea.expand").focus(function () {
-            $(this).animate({ rows: "15" }, 200);
-        });
+	$(document).ready(function () {
+		$("textarea.expand").focus(function () {
+			$(this).animate({ rows: "15" }, 200);
+		});
 
-        $("#javadoc-to-markdown-language").click(function () {
-            setLanguage($(this).val());
-        });
+		$("#javadoc-to-markdown-language").click(function () {
+			setLanguage($(this).val());
+		});
 
-        $("#btn-start").click(function (e) {
-            startConverter();
-            e.preventDefault();
-        });
+		$("#btn-start").click(function (e) {
+			startConverter();
+			e.preventDefault();
+		});
 
-        $("#javadoc-to-markdown-form").submit(function (e) {
-            var code,
-                headingsLevel,
-                language,
-                resultsContainer;
-            language = $(this).find("#javadoc-to-markdown-language").val();
-            headingsLevel = parseInt($(this).find("#javadoc-to-markdown-headings").val());
-            code = $(this).find("#javadoc-to-markdown-code").val();
+		$("#javadoc-to-markdown-form").submit(function (e) {
+			var code,
+				headingsLevel,
+				language,
+				resultsContainer;
+			language = $(this).find("#javadoc-to-markdown-language").val();
+			headingsLevel = parseInt($(this).find("#javadoc-to-markdown-headings").val());
+			code = $(this).find("#javadoc-to-markdown-code").val();
 
-            showConvertedCode(language, headingsLevel, code);
+			showConvertedCode(language, headingsLevel, code);
 
-            resultsContainer = $("#results");
-            resultsContainer.scrollTop(0);
+			resultsContainer = $("#results");
+			resultsContainer.scrollTop(0);
 
-            scrollToElement(resultsContainer.parent());
+			scrollToElement(resultsContainer.parent());
 
-            e.preventDefault();
-        });
+			e.preventDefault();
+		});
 
-        $("#btn-options").click(function (e) {
-            $("#hidden-options").slideDown(400);
-            scrollToElement($("#javadoc-to-markdown-form"));
+		$("#btn-options").click(function (e) {
+			$("#hidden-options").slideDown(400);
+			scrollToElement($("#javadoc-to-markdown-form"));
 
-            e.preventDefault();
-        });
+			e.preventDefault();
+		});
 
-        $("#btn-select-all").click(function (e) {
-            var results = $("#results");
-            selectText(results[0]);
+		$("#btn-select-all").click(function (e) {
+			var results = $("#results");
+			selectText(results[0]);
 
-            e.preventDefault();
-        });
-    });
+			e.preventDefault();
+		});
+	});
 
-    function scrollToElement(element) {
-        $("html, body").animate({
-            scrollTop: element.offset().top
-        }, 400);
-    }
+	function scrollToElement(element) {
+		$("html, body").animate({
+			scrollTop: element.offset().top
+		}, 400);
+	}
 
-    /**
-     * @author Jason (Stack Overflow)
-     */
-    function selectText(element) {
-        var doc = document,
-            range,
-            selection;
+	/**
+	 * @author Jason (Stack Overflow)
+	 */
+	function selectText(element) {
+		var doc = document,
+			range,
+			selection;
 
-        if (doc.body.createTextRange) { // MS
-            range = doc.body.createTextRange();
-            range.moveToElementText(element);
-            range.select();
-        }
-        else if (window.getSelection) { // all others
-            selection = window.getSelection();
-            range = doc.createRange();
-            range.selectNodeContents(element);
-            selection.removeAllRanges();
-            selection.addRange(range);
-        }
-    }
+		if (doc.body.createTextRange) { // MS
+			range = doc.body.createTextRange();
+			range.moveToElementText(element);
+			range.select();
+		}
+		else if (window.getSelection) { // all others
+			selection = window.getSelection();
+			range = doc.createRange();
+			range.selectNodeContents(element);
+			selection.removeAllRanges();
+			selection.addRange(range);
+		}
+	}
 
 })(jQuery);
