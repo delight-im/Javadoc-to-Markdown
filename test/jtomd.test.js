38cd8cd55ee727c816773b116f2a4b79708b327c
Layton Whiteley <layton.whiteley@gmail.com>
2017-05-07 14:14:54 -0500

    chore: cleanup

--- test/jtomd.test.js
+++ test/jtomd.test.js
@@ -23,37 +23,32 @@ describe('JavadocToMarkdown Suite: ', function() {
 
     describe('phpdoc Suite: ', function() {   
         it('should convert phpdoc to markdown when valid file is passed', function() {
-            var javaFile = fs.readFileSync('./examples/PHPDoc.txt', 'utf8');
+            var file = fs.readFileSync('./examples/PHPDoc.txt', 'utf8');
             var expected = fs.readFileSync('./examples/PHPDoc.expected.txt', 'utf8');
 
-            var output = javadocToMarkdown.convertCode('phpdoc', javaFile, 1);
+            var output = javadocToMarkdown.convertCode('phpdoc', file, 1);
             output.should.be.eql(expected);
-
         });   
     });
 
     describe('jsdoc Suite: ', function() {   
         it('should convert jsdoc to markdown when valid file is passed', function() {
-            var javaFile = fs.readFileSync('./examples/JSDoc.txt', 'utf8');
+            var file = fs.readFileSync('./examples/JSDoc.txt', 'utf8');
             var expected = fs.readFileSync('./examples/JSDoc.expected.txt', 'utf8');
 
-            var output = javadocToMarkdown.convertCode('JSDoc', javaFile, 1);
+            var output = javadocToMarkdown.convertCode('JSDoc', file, 1);
             output.should.be.eql(expected);
-
         });   
     });
 
     describe('Error Cases: ', function() {   
         it('should convert javadoc to markdown when valid file is passed', function() {
-            var javaFile = fs.readFileSync('./examples/JSDoc.txt', 'utf8');
+            var file = fs.readFileSync('./examples/JSDoc.txt', 'utf8');
             var expected = fs.readFileSync('./examples/JSDoc.expected.txt', 'utf8');
 
-            // javadocToMarkdown.convertCode('JSDocsss', javaFile, 1).should.throws();
-            expect(javadocToMarkdown.convertCode.bind(javadocToMarkdown, 'JSDocsss', javaFile, 1))
+            expect(javadocToMarkdown.convertCode.bind(javadocToMarkdown, 'JSDocsss', file, 1))
             .to
-            .throw('Unsupported language JSDocsss')
-            // output.should.be.eql(expected);
-
+            .throw('Unsupported language JSDocsss');
         });   
     });
 
